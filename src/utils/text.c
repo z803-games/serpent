d973aba74e511a710e378fe77be1368aac399c32
Nom075 <graytonl@nycstudents.net>
2026-07-23 10:54:33 -0400

    fix(text): correct draw_text function return type

--- src/utils/text.c
+++ src/utils/text.c
@@ -45,7 +45,7 @@ uint8_t get_character_vram_tile(char character) {
     return vram_tile;
 }
 
-uint8_t draw_text(char *text, uint8_t start_tile_x, uint8_t start_tile_y) {
+uint8_t *draw_text(char *text, uint8_t start_tile_x, uint8_t start_tile_y) {
     uint8_t *vram_address = get_bkg_xy_addr(start_tile_x, start_tile_y);
 
     for (uint8_t i = 0; text[i] != '\0'; ++i) {
--- src/utils/text.h
+++ src/utils/text.h
@@ -3,4 +3,4 @@
 #include <stdint.h>
 
 uint8_t get_character_vram_tile(char character);
-uint8_t draw_text(char *text, uint8_t start_tile_x, uint8_t start_tile_y);
+uint8_t *draw_text(char *text, uint8_t start_tile_x, uint8_t start_tile_y);
